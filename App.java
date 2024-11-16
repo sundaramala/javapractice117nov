d3eb89d290a7caf1bc8fb2891af7f8cfec0f7187
sundaramala <110267791+sundaramala@users.noreply.github.com>
2024-11-17 02:25:18 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,7 +1,13 @@
-package collection_list.Collection_practice;
+package Apache_program.AutomationNotesJavaSelenium;
 
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
